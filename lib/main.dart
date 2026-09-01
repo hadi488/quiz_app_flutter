fa5f05146aaff6fc5df6bb0a23eb085a67cd0943
Haider <haiderrehman509@gmail.com>
2026-09-01 14:54:25 +0500

    separate question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizler_flutter_app/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -32,6 +33,33 @@ class _QuizPageState extends State<QuizPage> {
     Icon(Icons.check, color: Colors.green),
     Icon(Icons.close, color: Colors.red),
   ];
+
+  // 'Q1 : \n You can lead a cow down stairs but not up stairs. false',
+  // 'Q2 : \n Approximately one quarter of human bones are in the feet. true',
+  // 'Q3 : \n A slug\'s blood is green. true',
+  // List<String> questions = [
+  //   'Q1 : \n You can lead a cow down stairs but not up stairs.',
+  //   'Q2 : \n Approximately one quarter of human bones are in the feet.',
+  //   'Q3 : \n A slug\'s blood is green.',
+  //   'End of Quiz',
+  // ];
+  // List<bool> answers = [false, true, true];
+
+  List<Question> questionList = [
+    Question(
+      q: 'Q1: \nYou can lead a cow down stairs but not up stairs.',
+      a: false,
+    ),
+    Question(
+      q: 'Q2: \nApproximately one quarter of human bones are in the feet.',
+      a: true,
+    ),
+    Question(q: 'Q3: \nA slug\'s blood is green.', a: true),
+    Question(q: 'End of Quiz', a: true),
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,7 +72,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionList[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -61,11 +89,16 @@ class _QuizPageState extends State<QuizPage> {
                 style: TextStyle(color: Colors.white, fontSize: 20.0),
               ),
               onPressed: () {
-                print(scoreKeeper.length);
+                bool correctAnswer = questionList[questionNumber].answer;
+                if (correctAnswer == true) {
+                  print('User got it right');
+                } else {
+                  print('User got it wrong');
+                }
                 setState(() {
-                  scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                  if (questionNumber < questionList.length - 1)
+                    questionNumber++;
                 });
-                print(scoreKeeper.length);
               },
             ),
           ),
@@ -80,15 +113,21 @@ class _QuizPageState extends State<QuizPage> {
                 style: TextStyle(fontSize: 20.0, color: Colors.white),
               ),
               onPressed: () {
+                bool correctAnswer = questionList[questionNumber].answer;
+                if (correctAnswer == false) {
+                  print('User got it right');
+                } else {
+                  print('User got it wrong');
+                }
                 setState(() {
-                  scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                  if (questionNumber < questionList.length - 1)
+                    questionNumber++;
                 });
-                print(scoreKeeper.length);
               },
             ),
           ),
         ),
-        Row(children: scoreKeeper),
+        // Row(children: scoreKeeper),
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  String questionText = '';
+  bool answer = false;
+  Question({required String q, required bool a}) {
+    questionText = q;
+    answer = a;
+  }
+}
